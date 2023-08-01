c145e4e0849cc58eb50f194ff43045c0e5c34adb
didymus-didymus <didymus-didymus@users.noreply.github.com>
2023-08-01 22:34:02 +0200

    exception handling

--- ConvertGbFile.py
+++ ConvertGbFile.py
@@ -4,13 +4,15 @@ import sys
 import os
 import subprocess
 import shutil
+import traceback
 
 rootPath = '/home/picoPad/picopad-playground/picopad-sdk/'
 buildPath = rootPath + 'picopad-gb/'
 
-yellowText = "\033[1;33;40m "
-greenText  = "\033[1;32;40m "
-normalText = "\033[0;37;40m "
+yellowText = "\033[1;33;40m"
+greenText  = "\033[1;32;40m"
+normalText = "\033[0;37;40m"
+redText    = "\033[0;31;40m"
 
 def convertFile(fileName):
     basePath = os.path.splitext(os.path.basename(fileName))[0]
@@ -30,18 +32,35 @@ def convertFile(fileName):
                 rom_c.write(line)
     os.remove(tempFile)
     print(yellowText+'MAKE execution:\n'+normalText)
-    subprocess.check_call(['make'], cwd=buildPath)
+    try:
+        subprocess.check_output(['make'], cwd=buildPath, stderr=subprocess.STDOUT, shell=True)
+    except subprocess.CalledProcessError as e:
+        errInfo=e.output.decode()
+        reportError(fileName, errInfo)
+        return
 
     binaryPath = rootPath + 'build/'
     
     head, tail = os.path.split(fileName)
-    shutil.copyfile(binaryPath+'PICOPAD-GB.PP2', f"{head}/{basePath}.PP2")
+    squeezedFileName = basePath.replace(" ", "").replace(")","").replace("(","").replace("-","").replace(",","")
+    shutil.copyfile(binaryPath+'PICOPAD-GB.PP2', f"{head}/{squeezedFileName}.PP2")
     os.remove(binaryPath+'PICOPAD-GB.PP2')
-    print(yellowText+f" FINISHED {basePath}\n"+normalText)
+    print(yellowText+f"FINISHED as {squeezedFileName}.PP2\n"+normalText)
+
+def reportError(fileName, ex):
+    print(f"{redText}Error in {fileName}{normalText}\n{ex}\n")
+    basePath = os.path.splitext(fileName)[0]
+    f = open(f"{basePath}.errorInfo.txt", "w+")
+    f.write(ex)
+    f.close()
 
 for subdir, dirs, files in os.walk(sys.argv[1]):
     for file in files:
         filepath = subdir + os.sep + file
 
         if filepath.endswith(".gb") or filepath.endswith(".gbc"):
-            convertFile (filepath)
+            try:
+                convertFile (filepath)
+            except:
+                var = traceback.format_exc()
+                reportError (filepath, var)
