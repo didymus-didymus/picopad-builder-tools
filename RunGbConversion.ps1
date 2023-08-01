8a19d31ede02ef3a64a19bf3dac61b9b01569624
didymus-didymus <didymus-didymus@users.noreply.github.com>
2023-08-01 22:33:12 +0200

    remove container after run

--- RunGbConversion.ps1
+++ RunGbConversion.ps1
@@ -1,2 +1,2 @@
 docker pull didymusdidymus/picopad-builder-autorun:latest
-docker run -v ${PWD}:/GB-FILES didymusdidymus/picopad-builder-autorun:latest
+docker run --rm -v ${PWD}:/GB-FILES didymusdidymus/picopad-builder-autorun:latest
